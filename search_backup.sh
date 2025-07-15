98e2e10375f3be33033c368bbca397107f408f67
Muteeba4626 <muteeba.shahzad26@gmail.com>
2025-07-15 06:55:53 +0000

    Update search_backup.sh

--- search_backup.sh
+++ search_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "------------FILE MANAGEMENT SYSTEM---------------------"
 
 SRC_DIR="./data"
-DO_INCREMENTAL=false
-DO_REPORT=false
+DO_INCREMENTAL=false    #-i flag 
+DO_REPORT=false         #-r flag 
+BACKUP_TYPE=""
 
 while getopts ":d:ir" opt; do
   case $opt in
@@ -27,6 +27,11 @@ PREV_FOLDER=$(ls -d backups/incremental-* 2>/dev/null | sort | tail -n 1)
 mkdir -p logs backups
 > "$TMP"
 
+printf "\n"
+echo "_________Enhanced Backup Service_________"
+printf "\n"
+
+
 read -p "Enter keyword to search: " KEY
 
 for f in $(find "$SRC_DIR" -type f); do
@@ -35,7 +40,7 @@ for f in $(find "$SRC_DIR" -type f); do
     echo "$REL_PATH" >> "$TMP"
   fi
 done
-
+printf "\n"
 if [ -s "$TMP" ]; then
   echo "Files containing '$KEY':"
   cat "$TMP"
@@ -46,17 +51,18 @@ else
   rm -f "$TMP"
   exit 1
 fi
-
+printf "\n"
 read -p "Backup matched files (tar)? (y/n): " ans_tar
 if [[ "$ans_tar" =~ ^[Yy]$ ]]; then
   tar -czf "$ARCHIVE" -C "$SRC_DIR" -T "$TMP"
   echo "Archived files to $ARCHIVE"
   echo "$TIME | Tar backup created: $ARCHIVE" >> "$LOG"
+  BACKUP_TYPE="Tar"
 else
   echo "Tar backup skipped"
   echo "$TIME | Tar backup skipped" >> "$LOG"
 fi
-
+printf "\n"
 read -p "Backup matched files (incremental rsync)? (y/n): " ans_rsync
 if [[ "$ans_rsync" =~ ^[Yy]$ ]]; then
   mkdir -p "$BACKUP_FOLDER"
@@ -65,8 +71,10 @@ if [[ "$ans_rsync" =~ ^[Yy]$ ]]; then
   else
     rsync -a --files-from="$TMP" "$SRC_DIR/" "$BACKUP_FOLDER/"
   fi
+  printf "\n"
   echo "Incremental backup saved to $BACKUP_FOLDER"
   echo "$TIME | Rsync incremental backup: $BACKUP_FOLDER" >> "$LOG"
+  BACKUP_TYPE="Incremental"
 else
   echo "Rsync incremental backup skipped"
   echo "$TIME | Rsync incremental backup skipped" >> "$LOG"
@@ -75,10 +83,18 @@ fi
 if [ "$DO_REPORT" = true ]; then
   COUNT=$(wc -l < "$TMP")
   SIZE=$(du -ch $(< "$TMP" sed "s|^|$SRC_DIR/|") 2>/dev/null | grep total$ | awk '{print $1}')
-  echo "$COUNT files archived, total $SIZE" > report.txt
+  {
+    echo "Daily Backup Report - $(date '+%Y-%m-%d')"
+    echo "================================"
+    echo "$COUNT files archived, total $SIZE"
+    echo "Source: $(realpath "$SRC_DIR")"
+    echo "Destination: $BACKUP_FOLDER"
+    echo "Backup Type: $BACKUP_TYPE"
+    echo "Timestamp: $TIME"
+  } > report.txt
   echo "$TIME | Report generated: report.txt" >> "$LOG"
 fi
-
+printf "\n"
 echo "Pushing to Git..."
 git checkout -B enhanced-backup
 git add .
@@ -91,7 +107,7 @@ if git commit -m "Backup on $DATE"; then
 else
   echo "Nothing new to commit. Tag not created."
 fi
-
+printf "\n"
 rm -f "$TMP"
 echo "Log: $LOG"
 echo "------------GOOD BYEEEE :>------------------------"
